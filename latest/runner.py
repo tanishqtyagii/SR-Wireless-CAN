866f86ce9d19b62742af50c8019f21b80f1b659b
Akash Karthik <akashkarthik473@gmail.com>
2026-03-17 17:57:27 -0700

    quick bug fix

--- latest/runner.py
+++ latest/runner.py
@@ -1,7 +1,7 @@
 # runner.py
 from CAN_controller import CANController
 from bootloader import bootload
-from hex_transfer import flash_hex
+from hex_transfer import hex_transfer
 from finalization import finalize
 from intelhex import IntelHex
 # import subprocess
